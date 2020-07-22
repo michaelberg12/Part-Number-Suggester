8f09c58d370444b6e5837cde5bfa025479fb6762
Michael Berg <urberg@yahoo.com>
2020-07-21 18:13:04 -0600

    finished part class

--- part.cpp
+++ part.cpp
@@ -1,1 +1,35 @@
 #include "part.h"
+
+Part::Part(std::string part_number, std::string part_id, std::string part_rev, std::string part_desc)
+{
+	_part_number = part_number;
+	_part_id = part_id;
+	_part_rev = part_rev;
+	_part_desc = part_desc;
+	_size = 10000;
+}
+
+void Part::set_size(int size)
+{
+	_size = size;
+}
+
+const char* Part::number()
+{
+	return std::string("<span size=\"" + std::to_string(_size)+ "\">" + _part_number + "</span>").c_str();
+}
+
+const char* Part::id()
+{
+	return std::string("<span size=\"" + std::to_string(_size) + "\">" + _part_id + "</span>").c_str();
+}
+
+const char* Part::rev()
+{
+	return std::string("<span size=\"" + std::to_string(_size) + "\">" + _part_rev + "</span>").c_str();
+}
+
+const char* Part::desc()
+{
+	return std::string("<span size=\"" + std::to_string(_size) + "\">" + _part_desc + "</span>").c_str();
+}
--- part.h
+++ part.h
@@ -6,14 +6,20 @@ class Part
 public:
 	Part(std::string part_number, std::string part_id, std::string part_rev, std::string part_desc);
 
-	char* number();
-	char* id();
-	char* rev();
-	char* desc();
+	void set_size(int size);
+
+	const char* number();
+	const char* id();
+	const char* rev();
+	const char* desc();
+
 private:
+
+	int _size;
 	std::string _part_number;
 	std::string _part_id;
 	std::string _part_rev;
 	std::string _part_desc;
+
 };
 
