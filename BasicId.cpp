7da62d321adf4248f917303d73fef126d1df2e6d
Michael Berg <urberg@yahoo.com>
2020-08-04 17:50:23 -0600

    added basic ID specifications

--- BasicId.cpp
+++ BasicId.cpp
@@ -1,1 +1,60 @@
 #include "BasicId.h"
+
+BasicId::BasicId()
+{
+}
+
+BasicId::BasicId(std::wstring file_name)
+{
+	id.assign(file_name.begin(), file_name.end());
+}
+
+std::string BasicId::to_str()
+{
+	return std::string();
+}
+
+std::wstring BasicId::to_wstr()
+{
+	return std::wstring();
+}
+
+std::string BasicId::type()
+{
+	return std::string();
+}
+
+std::string BasicId::rev_str()
+{
+	return std::string();
+}
+
+std::string BasicId::raw()
+{
+	return std::string();
+}
+
+std::string BasicId::new_id(std::string rev)
+{
+	return std::string();
+}
+
+bool BasicId::valid(std::wstring)
+{
+	return false;
+}
+
+bool BasicId::valid(std::string)
+{
+	return false;
+}
+
+bool BasicId::valid()
+{
+	return false;
+}
+
+int BasicId::size()
+{
+	return 0;
+}
--- BasicId.h
+++ BasicId.h
@@ -7,6 +7,7 @@
 class BasicId : public ID
 {
 public:
+	BasicId();
 	BasicId(std::wstring file_name);
 
 	//to display (parsed)
@@ -24,10 +25,29 @@ public:
 	//check validity
 	bool valid(std::wstring);
 	bool valid(std::string);
+	bool valid();
 
 	//the size of the id structure
 	int size();
 private:
+	/*
+	===================================ID STRUCTURE============================================
+	Example 37G103AA
+	        01234567
 
+	37 - The 0 and 1 space are numbers from 0 -> 9 and is random      (R)
+	G - The 2 space is any letter from A -> Z and is random           (R)
+	103 - The 3, 4, and 5 space are numbers from 0 -> 9 and is random (R)
+	AA - The 6 and 7 space are from A->Z and are not random 
+
+		The letters are incrimented in a base 26 fasion 
+		The letters indicates the revision
+		There may be no revision letters and that indicates the master copy
+
+	(R) - Required
+	===========================================================================================
+	*/
+
+	std::string id;
 };
 
